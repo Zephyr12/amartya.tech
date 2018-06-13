065ce1037d9b8ef268b37c2af564e4eaf62161fe
Amartya Vadlamani <amartya.vadlamani@gmail.com>
2018-06-13 17:36:23 +0100

    enhanced the metadata each template can use

--- src/rssg.py
+++ src/rssg.py
@@ -1,4 +1,5 @@
 import yaml
+import pdb
 import jinja2
 import markdown
 
@@ -14,10 +15,12 @@ SITE_NAME = "Amartya's Site"
 
 categories = next(walk(FILE_ROOT))[1]
 
-site_pages = {category: [filename
-                         for filename in listdir(join(FILE_ROOT, category)) 
-                         if isfile(join(FILE_ROOT, category, filename))]
-                for category in categories}
+site_pages = {category: {filename: yaml.load(open(join(FILE_ROOT, category, filename)).read().split("---\n")[0]) if category != "static" else {}
+                         for filename in listdir(join(FILE_ROOT, category))
+                         if isfile(join(FILE_ROOT, category, filename))}
+                for category in categories if category not in ["templates", "ignore"]}
+print(site_pages)
+base_variables =  {"site_pages": site_pages, "site_name": SITE_NAME}
 
 for content in listdir(OUTPUT_ROOT):
     shutil.rmtree(join(OUTPUT_ROOT, content))
@@ -26,21 +29,20 @@ env = jinja2.Environment(
         loader=jinja2.loaders.FileSystemLoader(join(FILE_ROOT, "templates")),
         autoescape=jinja2.select_autoescape([]))
 
-for category, files in site_pages.items():
+for category, document_set in site_pages.items():
     if category == "static":
         shutil.copytree(join(FILE_ROOT, "static"), join(OUTPUT_ROOT, "static"))
     elif category not in ["templates", "ignore"]:
         makedirs(join(OUTPUT_ROOT, category))
         template = env.get_template(category + ".html")
-        for file in files:
-            if not file.startswith("."):
-                print(file)
-                documents = [doc for doc in open(join(FILE_ROOT, category, file)).read().split("---\n")]
-                metadata = yaml.load(documents[0])
-                variables = {"site_pages": site_pages, "site_name": SITE_NAME, **metadata}
-                documents = [jinja2.Template(markdown.markdown(document)).render(variables) for document in documents[1:]]
+        for document_name, metadata in document_set.items():
+            if not document_name.startswith("."):
+                print(document_name)
+                documents = [doc for doc in open(join(FILE_ROOT, category, document_name)).read().split("---\n")]
+                variables = {**base_variables, **metadata}
+                documents = [markdown.markdown(jinja2.Template(document).render(variables)) for document in documents[1:]]
                 variables["documents"] = documents
-                with open(join(OUTPUT_ROOT, category, file.split(".")[0] + ".html"), "w+") as outfile:
+                with open(join(OUTPUT_ROOT, category, document_name.split(".")[0] + ".html"), "w+") as outfile:
                     outfile.write(template.render(**variables))
     else:
         pass
